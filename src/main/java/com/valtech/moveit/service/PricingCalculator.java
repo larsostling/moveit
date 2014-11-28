beeb8e54b6674e36167e3cc6d728e5b132731296
Lars Ostling <lars.ostling@gmail.com>
2014-11-28 20:31:35 +0100

    Implemented price calculation based on distance

--- src/main/java/com/valtech/moveit/service/PricingCalculator.java
+++ src/main/java/com/valtech/moveit/service/PricingCalculator.java
@@ -4,6 +4,16 @@ import com.valtech.moveit.model.Quotation;
 
 public class PricingCalculator {
     public int calculatePriceForQuotation(Quotation quotation) {
-        return 0;
+        return calculatePriceForDistance(quotation.getDistanceInKilometers());
+    }
+
+    private int calculatePriceForDistance(int distance) {
+        if (distance < 50) {
+            return 1000 + 10 * distance;
+        } else if (distance < 100) {
+            return 5000 + 8 * distance;
+        } else {
+            return 10000 + 7 * distance;
+        }
     }
 }
