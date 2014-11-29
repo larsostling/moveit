8dd24f8b9d4d31ca327363f0060d91ce05443759
Lars Ostling <lars.ostling@gmail.com>
2014-11-29 10:51:43 +0100

    Implement price calculation based on area

--- src/main/java/com/valtech/moveit/service/PricingCalculator.java
+++ src/main/java/com/valtech/moveit/service/PricingCalculator.java
@@ -4,7 +4,10 @@ import com.valtech.moveit.model.Quotation;
 
 public class PricingCalculator {
     public int calculatePriceForQuotation(Quotation quotation) {
-        return calculatePriceForDistance(quotation.getDistanceInKilometers());
+        int pricePerCar = calculatePriceForDistance(quotation.getDistanceInKilometers());
+        int numberOfCars =
+                calculateNumberOfCars(quotation.getAreaInSquareMeters(), quotation.getStorageAreaInSquareMeters());
+        return pricePerCar * numberOfCars;
     }
 
     private int calculatePriceForDistance(int distance) {
@@ -15,5 +18,10 @@ public class PricingCalculator {
         } else {
             return 10000 + 7 * distance;
         }
+    }
+
+    private int calculateNumberOfCars(int area, int storageArea) {
+        int totalArea = area + 2 * storageArea;
+        return 1 + totalArea / 50;
     }
 }
