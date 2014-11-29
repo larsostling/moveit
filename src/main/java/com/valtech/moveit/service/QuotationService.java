5bc3aff36b00f7c00d4a9dc578603d49aeaa7383
Lars Ostling <lars.ostling@gmail.com>
2014-11-29 14:53:09 +0100

    First implementaion of service for adding a quotation

--- src/main/java/com/valtech/moveit/service/QuotationService.java
+++ src/main/java/com/valtech/moveit/service/QuotationService.java
@@ -2,8 +2,22 @@ package com.valtech.moveit.service;
 
 import com.valtech.moveit.model.Quotation;
 
+import javax.inject.Inject;
+import java.util.UUID;
+
 public class QuotationService {
+
+    @Inject
+    PricingCalculator pricingCalculator;
+
     public Quotation addQuotation(Quotation quotation) {
-        return null;
+        quotation.setPrice(pricingCalculator.calculatePriceForQuotation(quotation));
+        quotation.setQuotationId(generateQuotationId());
+        return quotation;
+    }
+
+    private String generateQuotationId() {
+        String quotationId = UUID.randomUUID().toString();
+        return quotationId.replaceAll("-", "");
     }
 }
--- src/test/java/com/valtech/moveit/service/QuotationServiceTest.java
+++ src/test/java/com/valtech/moveit/service/QuotationServiceTest.java
@@ -3,18 +3,27 @@ package com.valtech.moveit.service;
 import com.valtech.moveit.model.Quotation;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.anyObject;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class QuotationServiceTest {
+
+    private static final int EXPECTED_PRICE = 1100;
 
     QuotationService underTest;
 
     @Before
     public void setUp() {
         underTest = new QuotationService();
+        PricingCalculator calculatorMock = mock(PricingCalculator.class);
+        when(calculatorMock.calculatePriceForQuotation(anyObject())).thenReturn(EXPECTED_PRICE);
+        underTest.pricingCalculator = calculatorMock;
     }
 
     @Test
@@ -25,7 +34,7 @@ public class QuotationServiceTest {
         assertThat(addedQuotation.getAreaInSquareMeters(), is(quotation.getAreaInSquareMeters()));
         assertThat(addedQuotation.getStorageAreaInSquareMeters(), is(quotation.getStorageAreaInSquareMeters()));
         assertThat(addedQuotation.isIncludePiano(), is(quotation.isIncludePiano()));
-        assertThat(addedQuotation.getPrice(), is(1100));
+        assertThat(addedQuotation.getPrice(), is(EXPECTED_PRICE));
         assertThat(addedQuotation.getQuotationId(), is(notNullValue()));
     }
 }
